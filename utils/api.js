942f2037bcb301c60d869be3bc29d888c5b79645
gotoart <kr@pinkman.ru>
2022-06-28 15:25:35 +0300

    api

--- utils/api.js
+++ utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let url = "http://0.0.0.0:5776/api/v2/";
 if (process.env.NODE_ENV !== "development") {
-  url = 'https://tapi.futbot.me/api/v1/'
+  url = 'https://newsapi.simplr.ru/api/v2/'
 }
 
 const APIConfig = axios.create({
