93550340bd8150b9fe3f7fb4b8ad0a2fb2c7d1ee
gotoart <kr@pinkman.ru>
2022-06-28 16:35:37 +0300

    gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {useEffect} from 'react';
+import TagManager from 'react-gtm-module';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    useEffect(() => {
+        TagManager.initialize({gtmId: 'GTM-TQC9N4D'});
+    }, []);
+    return <Component {...pageProps} />
 }
 
 export default MyApp
