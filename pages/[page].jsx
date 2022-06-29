1e3023ce236f2f05c10aa61c1b08036231d98ff7
gotoart <kr@pinkman.ru>
2022-06-29 13:39:41 +0300

    paginations

--- pages/[page].jsx
+++ pages/[page].jsx
@@ -5,14 +5,20 @@ import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import APIConfig from "../utils/api";
 import Head from 'next/head'
+import {useRouter} from 'next/router';
+
 
 const HomePage = ({newsList, rowsList}) => {
-     const onScroll = (event) => {
+    const {asPath, pathname} = useRouter();
+    const onScroll = (event) => {
         const target = event.target
         if (target.scrollHeight.toFixed() - target.scrollTop.toFixed() === target.clientHeight) {
-            window.location.replace(`/1`)
+            const currentPage = asPath.split('/')[1]
+            let nextPage = parseInt(currentPage) + 1
+            window.location.replace(`/${nextPage}`)
         }
     };
+
     return (
         <>
             <Head>
@@ -32,7 +38,7 @@ const HomePage = ({newsList, rowsList}) => {
             </Head>
 
 
-            <div className={'container news__wrapper scroller__wrapper'} onScroll={onScroll} id={'page-wrap'}>
+            <div className={'container news__wrapper scroller__wrapper'} onScroll={onScroll}>
                 <Header/>
 
                 <div className={'row'}>
@@ -47,12 +53,12 @@ const HomePage = ({newsList, rowsList}) => {
                                     newsPubDate={newsList[0].publication_date}
                                     newsSlug={newsList[0].slug}
                                 />
-                                <RegNewsList rowsList={rowsList} />
+                                <RegNewsList rowsList={rowsList}/>
                             </>
                             : null
                         }
                     </div>
-                    <Sidebar />
+                    <Sidebar/>
                 </div>
 
 
@@ -61,8 +67,31 @@ const HomePage = ({newsList, rowsList}) => {
     )
 }
 
-export async function getStaticProps() {
-    const res = await APIConfig.get(`get_last_news?offset=0&limit=26`);
+
+export async function getStaticPaths() {
+    return {
+        paths: [
+            {params: {page: '9'}},
+            {params: {page: '1'}},
+            {params: {page: '2'}},
+            {params: {page: '3'}},
+            {params: {page: '4'}},
+            {params: {page: '5'}},
+            {params: {page: '6'}},
+            {params: {page: '7'}},
+            {params: {page: '8'}},
+            {params: {page: '10'}},
+
+        ],
+        fallback: true
+    };
+}
+
+export async function getStaticProps({params}) {
+    const limit = 26
+    const offset = params.page * limit
+    console.log(`get_last_news?offset=${offset}}&limit=${limit}`)
+    const res = await APIConfig.get(`get_last_news?offset=${offset}&limit=${limit}`);
     const news = res.data;
     const rows = [...Array(Math.ceil((res.data.news.length / 2)))]
     const newsRows = rows.map((row, idx) => res.data.news.slice(idx * 2, idx * 2 + 2))
