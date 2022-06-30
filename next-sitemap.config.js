c56568a4f74d9d8aa02b2cc4db75453fa2a99102
gotoart <kr@pinkman.ru>
2022-06-30 14:10:34 +0300

    fix module export

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
+
 const config = {
   siteUrl: process.env.SITE_URL || 'https://hotbuzz.ru/',
   generateRobotsTxt: true
 }
 
-export default config
+module.exports = config
